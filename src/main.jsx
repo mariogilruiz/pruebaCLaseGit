23cc6f1d4388703b414ab67c987b3f8527ef1a6a
freelivecode <mariogilruiz66@gmail.com>
2024-04-03 16:47:51 +0200

    preDEvs

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
